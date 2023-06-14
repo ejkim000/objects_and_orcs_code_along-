5ee4196d651de5abc74988cfd13da7eb456cfca5
ejkim000 <developer.ejk@gmail.com>
2023-06-14 11:23:23 -0400

    created an obj

---
+++ app.js
@@ -0,0 +1,11 @@
+// Use array inside an object
+
+const adventurer = {
+    name: "EJ",
+    hitPoints: 10,
+    belongings: ["sword", "shield", "helmet"]
+}
+
+// access the values in the array
+
+console.log(adventurer.belongings);
