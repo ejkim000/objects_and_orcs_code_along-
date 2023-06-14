1a20e9eb6e2d7a0b614f0feb7b9075e5724b91ed
ejkim000 <developer.ejk@gmail.com>
2023-06-14 11:34:39 -0400

    obj in obj

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 // Use array inside an object
-
 const adventurer = {
     name: "EJ",
     hitPoints: 10,
-    belongings: ["sword", "shield", "helmet"]
+    belongings: ["sword", "shield", "helmet", "keys"],
+    companion: {
+        name: "Strawberry",
+        type: "human"
+    }
 }
 
 // access the values in the array
-
 console.log(adventurer.belongings);
+
+// access the first belonging
+console.log(`First belonging: ${adventurer.belongings[0]}`); // blaket notation
+
+// Iterate over an array that is within an object
+for (let i=0; i < adventurer.belongings.length; i++) {
+    console.log(adventurer.belongings[i]);
+}
+
+
+//  #1 Obj within Obj
+console.log(`My companion's name is ${adventurer.companion.name}`); // dot notation
+
